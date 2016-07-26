a20455fb52944384a8df88d1da396c64d023f6b7
malaklopez <malaklopez@scriptmedia.biz>
2016-07-25 21:59:06 -0700

    # Changed call search from just local to tolllfree in call_tracking/utils.py

--- call_tracking/utils.py
+++ call_tracking/utils.py
@@ -12,7 +12,7 @@ def search_phone_numbers(area_code=None):
     """Queries the Twilio REST API to get phone numbers available for puchase"""
     # You can change the country argument to search outside the US
     # area_code is an optional parameter
-    numbers = client.phone_numbers.search(area_code=area_code, country='US')
+    numbers = client.phone_numbers.search(type="tollfree", area_code=area_code, country='US')
 
     # Returns 30 by default - let's trim the list for UX purposes
     return numbers[:10]
@@ -22,6 +22,7 @@ def purchase_phone_number(phone_number):
     """Purchases a new phone number from the Twilio API"""
     # Use a TwiML Application SID so all our numbers use the same voice URL
     number = client.phone_numbers.purchase(
+        type='tollfree',
         phone_number=phone_number,
         voice_application_sid=settings.TWIML_APPLICATION_SID)
 
